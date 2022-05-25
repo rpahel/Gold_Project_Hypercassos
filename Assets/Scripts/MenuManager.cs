f775fd4e0340c7cc391b2d4dc5b707883367ac05
Romain <mromain911@gmail.com>
2022-05-25 14:21:57 +0200

    MaineMenu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string levelName;
+    [SerializeField]private GameObject optionMenu;
+    [SerializeField]private GameObject CloseMenu;
+    
+    
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    
+    public void OptionMenu()
+    {
+        if (!optionMenu.activeSelf)
+        {
+            optionMenu.SetActive(true);
+        }
+    }
+
+    public void CloseOptionMenu()
+    {
+        if (optionMenu.activeSelf)
+        {
+            optionMenu.SetActive(false);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
