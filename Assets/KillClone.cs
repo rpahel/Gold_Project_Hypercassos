cace820446ec51ff44df98ee5efbe888d18cdb78
rpahel <raphael.dacosta.pro@gmail.com>
2022-06-08 01:50:49 +0200

    bugfixes due to merge

--- Assets/KillClone.cs
+++ Assets/KillClone.cs
@@ -6,7 +6,7 @@ public class KillClone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<ASTITOUCH>().isClone)
+        if(collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<Player>().isClone)
         {
             Destroy(collision.gameObject);
         }
--- Assets/RandomTrash/Scripts_Trash/MultiClonage.cs
+++ Assets/RandomTrash/Scripts_Trash/MultiClonage.cs
@@ -19,7 +19,7 @@ public class MultiClonage : MonoBehaviour
             {
                 playerClone = Instantiate(playerPrefabs, target.position,Quaternion.identity);
                 playerClone.transform.GetChild(0).GetComponent<Player>().Speed +=5;
-                playerClone.transform.GetChild(0).GetComponent<ASTITOUCH>().isClone=true;
+                playerClone.transform.GetChild(0).GetComponent<Player>().isClone=true;
             }
             else
             {
