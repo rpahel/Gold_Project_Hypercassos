67dbdff3b6a358ac94bfbff64ef9efb81589adda
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-06-09 15:53:02 +0200

    tp dans le meme layer

--- Assets/RandomTrash/Scripts_Trash/SwitchLayer.cs
+++ Assets/RandomTrash/Scripts_Trash/SwitchLayer.cs
@@ -6,17 +6,20 @@ public class SwitchLayer : MonoBehaviour
 {
     private LevelBehaviour level;
     public bool upLayer;
+    public bool sameLayerTp;
     private bool cancoli;
     public GameObject tpTarget;
+    public BoxCollider2D boxCollider;
     private void Start()
     {
         level = FindObjectOfType<LevelBehaviour>();
         cancoli = true;
+        boxCollider = tpTarget.GetComponent<BoxCollider2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player" && cancoli)
+        if (collision.gameObject.tag == "Player")
         {
 
             if(tpTarget!=null)
@@ -24,7 +27,7 @@ public class SwitchLayer : MonoBehaviour
 
                 if (tpTarget.GetComponent<SwitchLayer>() != null)
                 {
-                    tpTarget.GetComponent<SwitchLayer>().StartCoroutine(coliTimer());
+                    StartCoroutine(coliTimer());
                 }
 
 
@@ -33,11 +36,17 @@ public class SwitchLayer : MonoBehaviour
                 Debug.Log("Tp");
                
             }
-            if (upLayer)
+            if (upLayer && !sameLayerTp)
             {
                 level.RequestLayerUp();
                 StartCoroutine(coliTimer());
                 collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 1000, ForceMode2D.Impulse);
+            }
+            else if (sameLayerTp)
+            {
+                
+                StartCoroutine(coliTimer());
+                collision.gameObject.transform.position = tpTarget.transform.position;                
             }
             else
             {
@@ -47,10 +56,13 @@ public class SwitchLayer : MonoBehaviour
         }
     }
 
-    IEnumerator coliTimer()
+    public IEnumerator coliTimer()
     {
-        cancoli = false;
+        Debug.Log("dans la couroutine");
+        //cancoli = false;
+        boxCollider.enabled = false;
         yield return new WaitForSeconds(1f);
+        boxCollider.enabled = true;
         cancoli = true;
     }
 }
