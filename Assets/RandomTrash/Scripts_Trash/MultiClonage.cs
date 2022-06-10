2f90b194fd114650e387d73009105b4489c4fe2b
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-06-10 11:43:50 +0200

    clone patch discover layer

--- Assets/RandomTrash/Scripts_Trash/MultiClonage.cs
+++ Assets/RandomTrash/Scripts_Trash/MultiClonage.cs
@@ -11,13 +11,22 @@ public class MultiClonage : MonoBehaviour
     
     private GameObject playerClone;
     private GameObject boxClone;
-    
-    
+    public int layerCount;
+    private GameObject levelObject;
+    private LevelBehaviour level;
+    private void Awake()
+    {
+        levelObject = FindObjectOfType<LevelBehaviour>().gameObject;
+        level = levelObject.GetComponent<LevelBehaviour>();
+        layerCount = level.currentLayer + 1;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
-            if(playerClone == null)
+            layerCount = level.currentLayer + 1;
+            level.levelLayers[layerCount].Discover();
+            if (playerClone == null)
             {
                 playerClone = Instantiate(playerPrefabs, target.position,Quaternion.identity);
                 playerClone.transform.GetChild(0).GetComponent<ASTITOUCH>().speed +=5;
