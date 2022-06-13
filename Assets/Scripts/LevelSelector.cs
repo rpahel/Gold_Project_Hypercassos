f4ce14bd48f3dffb40ecc2a957073b113e0e9042
Romain <mromain911@gmail.com>
2022-06-13 23:59:56 +0200

    unlock and lock level

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -8,77 +8,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
-    public GameObject levelHolder;
-    public GameObject levelIcon;
-    public GameObject thisCanvas;
-    public int numberofLevels = 5;
-    public Vector2 inconSpacing;
+    public Button[] lvlButtons;
 
-    private Rect panelDimensions;
-    private Rect iconDimensions;
-    private int amountPerPage;
-    private int currentLevelCount;
-    private int nIcons;
-    
-    
-    void Start()
+    private void Start()
     {
-        panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
-        iconDimensions = levelIcon.GetComponent<RectTransform>().rect;
+        int levelAt = PlayerPrefs.GetInt("levelAt", 2);
 
-        int maxInARow = Mathf.FloorToInt((panelDimensions.width + inconSpacing.x)/(iconDimensions.width + inconSpacing.x));
-        int maxInACol = Mathf.FloorToInt((panelDimensions.height +inconSpacing.y) / (iconDimensions.height + inconSpacing.y));
-
-        amountPerPage = maxInARow * maxInACol;
-        int totalPages = Mathf.CeilToInt((float)numberofLevels/amountPerPage);
-        LoadPanels(totalPages);
-        
-    }
-
-    private void LoadPanels(int nPanels)
-    {
-        GameObject panelClone =  Instantiate(levelHolder) as GameObject;
-
-        for (int i = 1; i <= nPanels; i++)
+        for (int i = 0; i < lvlButtons.Length; i++)
         {
-            GameObject panel = Instantiate(panelClone) as GameObject;
-            panel.transform.SetParent(thisCanvas.transform, false);
-            panel.transform.SetParent(levelHolder.transform);
-            panel.name = "Page-" + i;
-            panel.GetComponent<RectTransform>().localPosition = new Vector2(panelDimensions.width * (i - 1), 0);
-            SetupGrid(panel);
-            nIcons = i == nPanels ? numberofLevels - currentLevelCount : amountPerPage;
-            LoadIcons(ref nIcons, panel);
-            Debug.Log(nIcons);
-            
-        }
-        Destroy(panelClone);
-    }
-
-    private void SetupGrid(GameObject panel)
-    {
-       GridLayoutGroup grid = panel.AddComponent<GridLayoutGroup>();
-       grid.cellSize = new Vector2(iconDimensions.width, iconDimensions.height);
-       grid.childAlignment = TextAnchor.MiddleCenter;
-       grid.spacing = inconSpacing;
-    }
-    
-
-    protected virtual void LoadIcons(ref int nIcons, GameObject parentObject)
-    {
-        for (int i = 1; i <= nIcons; i++)
-        {
-            currentLevelCount++;
-            GameObject icon = Instantiate(levelIcon) as GameObject;
-            icon.transform.SetParent(thisCanvas.transform, false);
-            icon.transform.SetParent(parentObject.transform);
-            icon.name = "Level " + i;
-            icon.GetComponentInChildren<TextMeshProUGUI>().SetText("Level " + currentLevelCount);
-            
+            if (i + 2 > levelAt)
+                lvlButtons[i].interactable = false;
         }
     }
-
-    
-
-    
 }
