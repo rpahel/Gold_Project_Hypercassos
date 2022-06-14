cf93724408b1700189a88fcaff5a396c07898626
Romain <mromain911@gmail.com>
2022-06-14 22:16:23 +0200

    Mute Music

---
+++ Assets/Scripts/MuteManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteManager : MonoBehaviour
+{
+    private bool isMuted;
+    void Start()
+    {
+        isMuted = PlayerPrefs.GetInt("MUTED") == 1;
+        AudioListener.pause = isMuted;
+    }
+
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+        PlayerPrefs.SetInt("MUTED", isMuted ? 1 : 0);
+    }
+}
